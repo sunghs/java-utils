be6edcc39815aa2797a42775cb663fa025ee7281
sunghs <sunghs9071@naver.com>
2020-05-29 11:47:24 +0900

    Initial commit

---
+++ src/main/java/sunghs/java/utils/JavaUtilsApplication.java
@@ -0,0 +1,13 @@
+package sunghs.java.utils;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaUtilsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaUtilsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/sunghs/java/utils/JavaUtilsApplicationTests.java
@@ -0,0 +1,13 @@
+package sunghs.java.utils;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaUtilsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
