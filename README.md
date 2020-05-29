904a51e56450986e35ad089d3ac7a98e7b9ef511
sunghs <sunghs9071@naver.com>
2020-05-30 00:29:20 +0900

    매핑 치환 클래스 구현

---
+++ README.md
@@ -0,0 +1,10 @@
+#자바 복붙용 유틸 모음
+
+https://sunghs.tistory.com 에 있습니다.
+
+##프로젝트 환경은
+- JAVA 11
+- SPRING BOOT 2.3
+- GRADLE 6.3
+
+입니다.
--- src/main/java/sunghs/java/utils/string/StringMapper.java
+++ src/main/java/sunghs/java/utils/string/StringMapper.java
@@ -1,6 +1,75 @@
 package sunghs.java.utils.string;
 
+import java.util.Map;
+
+/**
+ * 문자열 내에 있는 ${key}로 된 매핑 규칙을 ValueMap의 key로 value를 가져와 치환합니다.
+ *
+ * @author SUNGHS
+ */
 public class StringMapper {
 
+    public static final String MAPPING_PREFIX = "${";
 
+    public static final String MAPPING_SUFFIX = "}";
+
+    public static final String EMPTY_STRING = "";
+
+    static class Cursor {
+
+        private int s;
+
+        private int e;
+
+        void setS(int s) {
+            this.s = s;
+        }
+
+        void setE(int e) {
+            this.e = e;
+        }
+
+        int getS() {
+            return this.s;
+        }
+
+        int getE() {
+            return this.e;
+        }
+    }
+
+    public static String doReplace(final String str, final Map<String, String> map) {
+        int offset = 0;
+        String replaced = str;
+
+        while (true) {
+            Cursor csr = find(replaced, offset);
+            int s = csr.getS();
+            int e = csr.getE();
+            if (s < 0 || e < 0) {
+                break;
+            } else {
+                String key = replaced.substring(s + MAPPING_PREFIX.length(), e);
+                String value = (map.get(key) == null ? EMPTY_STRING : map.get(key));
+
+                int vLen = value.length();
+                int oLen = MAPPING_PREFIX.length() + key.length() + MAPPING_SUFFIX.length();
+                int diff = vLen - oLen;
+
+                replaced = replaced.replace(MAPPING_PREFIX + key + MAPPING_SUFFIX, value);
+                offset = e + diff;
+            }
+        }
+        return replaced;
+    }
+
+    private static Cursor find(String source, int offset) {
+        int sid = source.indexOf(MAPPING_PREFIX, offset);
+        int eid = source.indexOf(MAPPING_SUFFIX, offset);
+
+        Cursor csr = new Cursor();
+        csr.setS(sid);
+        csr.setE(eid);
+        return csr;
+    }
 }
