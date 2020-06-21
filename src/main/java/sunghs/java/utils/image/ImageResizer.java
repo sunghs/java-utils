ee21fbbd5a2e90b3ff131a953db10973efdc9356
sunghs <sunghs9071@naver.com>
2020-06-21 15:09:40 +0900

    ImageResizer 추가 Lombok 추가

---
+++ src/main/java/sunghs/java/utils/image/ImageResizer.java
@@ -0,0 +1,34 @@
+package sunghs.java.utils.image;
+
+import java.awt.geom.AffineTransform;
+import java.awt.image.AffineTransformOp;
+import java.awt.image.BufferedImage;
+
+public class ImageResizer {
+
+    public static final int MAX_SCALE_SIZE = 10000;
+
+    /**
+     * @param image 원본
+     * @param w     리사이징 가로 (px)
+     * @param h     리사이징 세로 (px)
+     * @return reiszed buffered image
+     */
+    public BufferedImage resize(final BufferedImage image, int w, int h) {
+        final int ow = image.getWidth();
+        final int oh = image.getHeight();
+
+        w = Math.min(w, MAX_SCALE_SIZE);
+        h = Math.min(h, MAX_SCALE_SIZE);
+
+        double sw = (double) w / (double) ow;
+        double sh = (double) h / (double) oh;
+
+        BufferedImage resize = new BufferedImage(w, h, image.getType());
+
+        AffineTransform transForm = new AffineTransform();
+        transForm.scale(sw, sh);
+        AffineTransformOp transformOp = new AffineTransformOp(transForm, AffineTransformOp.TYPE_BICUBIC);
+        return transformOp.filter(image, resize);
+    }
+}
---
+++ src/test/java/sunghs/java/utils/image/ImageResizerTest.java
@@ -0,0 +1,26 @@
+package sunghs.java.utils.image;
+
+import org.junit.jupiter.api.Test;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.*;
+
+public class ImageResizerTest {
+
+    @Test
+    public void resizeTest() throws Exception {
+        File original = new File("/Users/sunghs/Documents/Example/docker-image.png");
+        InputStream in = new FileInputStream(original);
+
+        ImageResizer imageResizer = new ImageResizer();
+        int w = 1000;
+        int h = 1000;
+        BufferedImage resize = imageResizer.resize(ImageIO.read(in), w, h);
+
+        String option = w + "x" + h;
+        FileOutputStream out = new FileOutputStream("/Users/sunghs/Documents/Example/docker-image-" + option + ".png");
+        ImageIO.write(resize, "png", out);
+        out.flush();
+    }
+}
