2a33e98d96ba6d95954d39ec3b46972ef8c4e97a
sunghs <sunghs9071@naver.com>
2020-07-07 18:50:19 +0900

    SEED-128 추가

--- src/main/java/sunghs/java/utils/cipher/Seed128.java
+++ src/main/java/sunghs/java/utils/cipher/Seed128.java
@@ -1,4 +1,51 @@
 package sunghs.java.utils.cipher;
 
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
+import java.util.Optional;
+import java.util.function.Predicate;
+import sunghs.java.utils.cipher.support.KisaSeedCbc;
+
+/**
+ * SEED-128 암호화
+ *
+ * @author https://sunghs.tistory.com
+ * @see <a href="https://github.com/sunghs/java-utils">source</a>
+ */
 public class Seed128 {
+
+    private static final Charset ENCODING_TYPE = StandardCharsets.UTF_8;
+
+    private final byte[] key;
+
+    private final byte INIT_VECTOR[] = {
+        (byte) 0x026, (byte) 0x08d, (byte) 0x066, (byte) 0x0a7, (byte) 0x035, (byte) 0x0a8,
+        (byte) 0x01a, (byte) 0x081, (byte) 0x06f, (byte) 0x0ba, (byte) 0x0d9, (byte) 0x0fa,
+        (byte) 0x036, (byte) 0x016, (byte) 0x025, (byte) 0x001
+    };
+
+    public Seed128(final String key) {
+        validation(key);
+        this.key = key.getBytes(ENCODING_TYPE);
+    }
+
+    public String encrypt(final String str) {
+        byte[] strBytes = str.getBytes(ENCODING_TYPE);
+        byte[] encrypted = KisaSeedCbc.SEED_CBC_Encrypt(this.key, this.INIT_VECTOR, strBytes, 0, strBytes.length);
+        return new String(Base64.getEncoder().encode(encrypted), ENCODING_TYPE);
+    }
+
+    public String decrypt(final String str) {
+        byte[] decoded = Base64.getDecoder().decode(str.getBytes(ENCODING_TYPE));
+        byte[] decrypted = KisaSeedCbc.SEED_CBC_Decrypt(this.key, this.INIT_VECTOR, decoded, 0, decoded.length);
+        return new String(decrypted, ENCODING_TYPE);
+    }
+
+    private void validation(final String key) {
+        Optional.ofNullable(key)
+            .filter(Predicate.not(String::isBlank))
+            .filter(Predicate.not(s -> s.length() != 16))
+            .orElseThrow(IllegalArgumentException::new);
+    }
 }
---
+++ src/test/java/sunghs/java/utils/cipher/Seed128Test.java
@@ -0,0 +1,34 @@
+package sunghs.java.utils.cipher;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+public class Seed128Test {
+
+    @Test
+    public void test() {
+        Seed128 seed128 = new Seed128("password12345678");
+        String plain = "this is plain text. 가나다라마바사. 123456";
+        String enc = seed128.encrypt(plain);
+        String dec = seed128.decrypt(enc);
+
+        log.info(enc);
+        log.info(dec);
+
+        Assertions.assertEquals(plain, dec);
+    }
+
+    @Test
+    public void passwordOverLength() {
+        // 유저 키가 16자리 이상 인 경우 에러
+        Seed128 s1 = new Seed128("password1234567890qwertyui");
+    }
+
+    @Test
+    public void passwordUnderLength() {
+        // 유저 키가 16자리 이하 인 경우 에러
+        Seed128 s2 = new Seed128("password");
+    }
+}
