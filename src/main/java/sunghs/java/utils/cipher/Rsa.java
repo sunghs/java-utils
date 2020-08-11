26bdfda53d1e72692e196df031fbc6feaaffd1bf
sunghs <sunghs9071@naver.com>
2020-08-11 23:26:52 +0900

    RSA 암호화 추가

---
+++ src/main/java/sunghs/java/utils/cipher/Rsa.java
@@ -0,0 +1,127 @@
+package sunghs.java.utils.cipher;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.security.*;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.security.spec.X509EncodedKeySpec;
+import java.util.Base64;
+
+public class Rsa {
+
+    private static final Charset ENCODING_TYPE = StandardCharsets.UTF_8;
+
+    private static final String INSTANCE_TYPE = "RSA";
+
+    private final Class<?> keyClass;
+
+    private final Key keyInstance;
+
+    private Cipher cipher;
+
+    public Rsa(final Key key) {
+        if (key instanceof PublicKey) {
+            keyClass = PublicKey.class;
+            keyInstance = key;
+        } else if (key instanceof PrivateKey) {
+            keyClass = PrivateKey.class;
+            keyInstance = key;
+        } else {
+            throw new ClassCastException("unknown key class");
+        }
+
+        try {
+            cipher = Cipher.getInstance(INSTANCE_TYPE);
+        } catch (NoSuchPaddingException | NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * key 객체 String 형으로 인코딩
+     *
+     * @param key key
+     * @return String encodedKey
+     */
+    public static String encodeKey(final Key key) {
+        return new String(Base64.getEncoder().encode(key.getEncoded()));
+    }
+
+    /**
+     * String 으로 인코딩 된 private key 를 객체로 변환
+     *
+     * @param encodedKey encodedKey
+     * @return PrivateKey key
+     */
+    public static PrivateKey decodePrivateKey(final String encodedKey) {
+        try {
+            byte[] bKey = Base64.getDecoder().decode(encodedKey.getBytes(ENCODING_TYPE));
+            PKCS8EncodedKeySpec pkcs8EncodedKeySpec = new PKCS8EncodedKeySpec(bKey);
+            KeyFactory keyFactory = KeyFactory.getInstance(INSTANCE_TYPE);
+            return keyFactory.generatePrivate(pkcs8EncodedKeySpec);
+        } catch (NoSuchAlgorithmException | InvalidKeySpecException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    /**
+     * String 으로 인코딩 된 public key 를 객체로 변환
+     *
+     * @param encodedKey encodedKey
+     * @return PublicKey key
+     */
+    public static PublicKey decodePublicKey(final String encodedKey) {
+        try {
+            byte[] bKey = Base64.getDecoder().decode(encodedKey.getBytes(ENCODING_TYPE));
+            X509EncodedKeySpec x509EncodedKeySpec = new X509EncodedKeySpec(bKey);
+            KeyFactory keyFactory = KeyFactory.getInstance(INSTANCE_TYPE);
+            return keyFactory.generatePublic(x509EncodedKeySpec);
+        } catch (NoSuchAlgorithmException | InvalidKeySpecException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    /**
+     * PublicKey, PrivateKey 생성
+     *
+     * @param size 생성될 키의 지정 값 (비트 수), 1024, 2048 권장
+     * @return KeyPair
+     */
+    public static KeyPair generateKey(int size) {
+        try {
+            KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance(INSTANCE_TYPE);
+            keyPairGenerator.initialize(size);
+            return keyPairGenerator.generateKeyPair();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public String encrypt(final String str) throws InvalidKeyException, BadPaddingException, IllegalBlockSizeException {
+        if (keyClass.isAssignableFrom(PublicKey.class)) {
+            cipher.init(Cipher.ENCRYPT_MODE, keyInstance);
+            byte[] encrypted = cipher.doFinal(str.getBytes(ENCODING_TYPE));
+            return new String(Base64.getEncoder().encode(encrypted), ENCODING_TYPE);
+        } else {
+            throw new ClassCastException("not public key set");
+        }
+    }
+
+    public String decrypt(final String str) throws InvalidKeyException, BadPaddingException, IllegalBlockSizeException {
+        if (keyClass.isAssignableFrom(PrivateKey.class)) {
+            cipher.init(Cipher.DECRYPT_MODE, keyInstance);
+            byte[] decrypted = Base64.getDecoder().decode(str.getBytes(ENCODING_TYPE));
+            return new String(cipher.doFinal(decrypted), ENCODING_TYPE);
+        } else {
+            throw new ClassCastException("not private key set");
+        }
+    }
+}
---
+++ src/test/java/sunghs/java/utils/cipher/RsaTest.java
@@ -0,0 +1,59 @@
+package sunghs.java.utils.cipher;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.IllegalBlockSizeException;
+import java.security.InvalidKeyException;
+import java.security.KeyPair;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+import java.util.Optional;
+
+@Slf4j
+public class RsaTest {
+
+    @Test
+    public void createKeyPair() {
+        KeyPair keyPair = Rsa.generateKey(1024);
+        Optional.ofNullable(keyPair).ifPresent(k -> {
+            PublicKey publicKey = k.getPublic();
+            PrivateKey privateKey = k.getPrivate();
+
+            log.info("public : {}", Rsa.encodeKey(publicKey));
+            log.info("private : {}", Rsa.encodeKey(privateKey));
+        });
+    }
+
+    @Test
+    public void test() {
+        KeyPair keyPair = Rsa.generateKey(2048);
+        Optional.ofNullable(keyPair).ifPresent(k -> {
+            PublicKey publicKey = k.getPublic();
+            PrivateKey privateKey = k.getPrivate();
+
+            Rsa rsaForPublic = new Rsa(publicKey);
+            Rsa rsaForPrivate = new Rsa(privateKey);
+
+            /*
+            평문은 public key로 암호화하고, private key로 복호화 한다.
+
+            RSA 는 반대로 public 복호화, private 암호화가 가능하지만,
+            대부분 암호화 하는 키를 공개키(public)로, 복호화 하는 키를 개인키(private) 정의 하기 때문에
+            public으로 암호화, private으로 복호화 한다.
+             */
+
+            String plain = "test 1234567890 가나다라마바사아자차카타파하";
+
+            try {
+                String encrypted = rsaForPublic.encrypt(plain);
+                log.info("encrypted : {}", encrypted);
+                String decrypted = rsaForPrivate.decrypt(encrypted);
+                log.info("decrypted : {}", decrypted);
+            } catch (InvalidKeyException | BadPaddingException | IllegalBlockSizeException e) {
+                e.printStackTrace();
+            }
+        });
+    }
+}
