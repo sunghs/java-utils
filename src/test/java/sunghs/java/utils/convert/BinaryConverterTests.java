fbcc4cbf741f79ab34408c2c7afe4f90cf8ab023
sunghs <sunghs9071@naver.com>
2021-05-26 16:01:05 +0900

    파일 비교 추가

--- src/test/java/sunghs/java/utils/convert/BinaryConverterTests.java
+++ src/test/java/sunghs/java/utils/convert/BinaryConverterTests.java
@@ -4,9 +4,11 @@ import java.io.IOException;
 import java.io.RandomAccessFile;
 import java.nio.ByteBuffer;
 import java.nio.channels.FileChannel;
+import java.nio.channels.FileChannel.MapMode;
 import java.util.ArrayList;
 import java.util.List;
 import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 @Slf4j
@@ -16,7 +18,10 @@ class BinaryConverterTests {
 
     @Test
     void test() throws IOException {
-        RandomAccessFile randomAccessFile1 = new RandomAccessFile("/Users/sunghs/Downloads/test.zip", "r");
+        String src = "/Users/sunghs/Downloads/test.zip";
+        String dest = "/Users/sunghs/Downloads/result.zip";
+
+        RandomAccessFile randomAccessFile1 = new RandomAccessFile(src, "r");
         FileChannel fileChannel1 = randomAccessFile1.getChannel();
         ByteBuffer byteBuffer1 = ByteBuffer.allocate(64);
 
@@ -46,7 +51,7 @@ class BinaryConverterTests {
         // when (string -> byte)
         byte[] convertedBytes = binaryConverter.convertStringToBinary(byteStrings);
 
-        RandomAccessFile randomAccessFile2 = new RandomAccessFile("/Users/sunghs/Downloads/result.zip", "rw");
+        RandomAccessFile randomAccessFile2 = new RandomAccessFile(dest, "rw");
         FileChannel fileChannel2 = randomAccessFile2.getChannel();
         ByteBuffer byteBuffer2 = ByteBuffer.allocate(convertedBytes.length);
 
@@ -57,5 +62,32 @@ class BinaryConverterTests {
             fileChannel2.write(byteBuffer2);
         }
         randomAccessFile2.close();
+
+        // then
+        Assertions.assertTrue(compare(src, dest));
+    }
+
+    boolean compare(String src, String dest) {
+        try {
+            FileChannel sChannel = new RandomAccessFile(src, "r").getChannel();
+            FileChannel dChannel = new RandomAccessFile(dest, "r").getChannel();
+
+            if (sChannel.size() != dChannel.size()) {
+                return false;
+            }
+
+            ByteBuffer sBuffer = sChannel.map(MapMode.READ_ONLY, 0, sChannel.size());
+            ByteBuffer dBuffer = sChannel.map(MapMode.READ_ONLY, 0, sChannel.size());
+
+            for (int i = 0; i < sChannel.size(); i ++) {
+                if (sBuffer.get(i) != dBuffer.get(i)) {
+                    return false;
+                }
+            }
+            return true;
+        } catch (Exception e) {
+            log.error("error", e);
+            return false;
+        }
     }
 }
