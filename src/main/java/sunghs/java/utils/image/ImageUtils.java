60c56220c037e32a4a12f7ff8dd0e368fe9e02ef
sunghs <sunghs9071@naver.com>
2022-03-29 22:48:51 +0900

    image util 추가

---
+++ src/main/java/sunghs/java/utils/image/ImageUtils.java
@@ -0,0 +1,29 @@
+package sunghs.java.utils.image;
+
+import lombok.extern.slf4j.Slf4j;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+
+/**
+ * Image 관련 Utils
+ *
+ * @author https://sunghs.tistory.com
+ * @see <a href="https://github.com/sunghs/java-utils">source</a>
+ */
+@Slf4j
+public class ImageUtils {
+
+    public BufferedImage fileToBufferedImage(final File file) {
+        if (file == null) {
+            return null;
+        }
+        try {
+            return ImageIO.read(file);
+        } catch (Exception e) {
+            log.error("file to bufferedImage error", e);
+            return null;
+        }
+    }
+}
